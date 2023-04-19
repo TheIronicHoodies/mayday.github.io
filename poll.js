b61cef0913fead753c4bb102325c28cf097dc408
TheIronicHoodies <lawson.ann009@gmail.com>
2023-04-19 13:20:29 +0100

    Reworked code for poll. Cleaned & refactored code.

--- poll.js
+++ poll.js
@@ -1,36 +1,38 @@
+const fileSystem = require("browserify-fs");
 const form = document.querySelector('form'); // Selects the form.
-let selection = document.getElementById('crewmates'); // Grabs the selected option.
 
-var i, option;
-
-const onChange = () => {
-    // 
+let obj = {
+    "vote": document.getElementById('crewmates') // Grabs the selected option.
 }
-// This function is called upon pressing submit
+
+let jsonObj = JSON.stringify(obj);
+
+// Called upon pressing submit
 const submitVote = () => {
-    form.addEventListener('submit', (e) => {
+
+    form.addEventListener('submit', handleForm);
+
+    const handleForm = (e) => {
+
         e.preventDefault(); // Prevents the browser from refreshing;
-        saveVote();
+        e.stopImmediatePropagation();
+
+        saveVote(); // Saves the selected option as a JSON string
+
+        // Success message & pseudo console log
+        document.getElementById("returnMessage").innerHTML = "Your vote for the " + selection.vote + " has pushed through successfully."
         document.getElementById("returnMessage").style.display = "block";
+    }
 
-        let vote = selection.value; // The dropdown menu's 'value'; what is currently selected
-        json = JSON.stringify(vote);
-    })
+    const saveVote = () => {
+
+        fileSystem.writeFile("./votes.json", jsonObj, err => {
+            if (err) {
+                console.log("Error writing file", err)
+            } else {
+                console.log('JSON data is written to the file successfully')
+            }
+        })
+    }
 }
 
-const saveVote = () => {
-    const fs = require("fs");
-    fs.readFile("./votes.json", "utf8", (err, jsonstring) => {
-        if (err) {
-            // Something here that tells the user on the site that something's wrong and their input didn't go through
-            return;
-        }
-        try {
-            const user = JSON.parse(jsonstring);
-        } catch {
-            // Something here that tells the user on the site that something's wrong and their input didn't go through
-        }
-    })
-}
-
-document.getElementById("returnMessage").innerHTML = "Your vote for the " + selection + " has pushed through successfully."
