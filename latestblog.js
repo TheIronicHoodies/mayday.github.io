1267c9ff7c5b881952879e85c2a950ef01e5bf39
TheIronicHoodies <lawson.ann009@gmail.com>
2023-04-29 16:44:38 +0100

    updated BLOG experimenting with sidebar click for mobile friendliness

---
+++ latestblog.js
@@ -0,0 +1,4 @@
+function editlatest() {
+    document.getElementById("latest").href = "a bit about voting.html";
+    return false;
+}
--- sidebar.js
+++ sidebar.js
@@ -1,8 +1,10 @@
 function openSideBar() {
-    document.getElementById("sidemenu").style.width = "300px";
+    document.getElementById("sidemenu").style.width = "310px";
+    document.getElementById("sidemenu").style.left = "0px"
     document.getElementById("main").style.marginLeft = "0px";
 }
 function closeSideBar() {
     document.getElementById("sidemenu").style.width = "5px";
+    document.getElementById("sidemenu").style.left = "-295px"
     document.getElementById("main").style.marginLeft= "0px";
 }
