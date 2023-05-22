058ca94ee2d222293240fdd82078612326a5e719
TheIronicHoodies <lawson.ann009@gmail.com>
2023-05-22 12:35:07 +0100

    Update

--- latestchap.js
+++ latestchap.js
@@ -1,4 +1,4 @@
 function editlatestc() {
-    document.getElementById("latestc").href = "The Professor 2.html";
+    document.getElementById("latestc").href = "The Doctor 3.html";
     return false;
 }
