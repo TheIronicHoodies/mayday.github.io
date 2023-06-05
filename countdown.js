8c6c3aef12df8bccb1d9e5bf4a98e85685309afa
TheIronicHoodies <lawson.ann009@gmail.com>
2023-06-05 13:12:56 +0100

    Fixed Chapterlist

--- countdown.js
+++ countdown.js
@@ -11,6 +11,10 @@ var countdown = setInterval(function() {
     var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
+    if(timeleft <= 0){
+        // add 7 days to Countdown Date
+    }
+
     // Output
     document.getElementById("days").innerHTML = days + "d "
     document.getElementById("hours").innerHTML = hours + "h " 
