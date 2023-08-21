2b1f643704cf3ad52d978962905d08843bed486e
TheIronicHoodies <lawson.ann009@gmail.com>
2023-08-21 20:05:49 +0100

    Increased countdown

--- countdown.js
+++ countdown.js
@@ -1,4 +1,4 @@
-var countdownDate = new Date("August 21, 2023 12:00:00").getTime(); // Countdown until date
+var countdownDate = new Date("August 28, 2023 12:00:00").getTime(); // Countdown until date
 
 // Runs countdown every second
 var countdown = setInterval(function() {
